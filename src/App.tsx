adf46d4e9edc483a9fb3775bdf4f566f88f0ba03
Igor <danieligord7@hotmail.com>
2022-10-21 21:48:04 -0300

    DEV: testing deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
 
-import GlobalStructure from './Components/Structurals/GlobalStructure.component';
+import styled from 'styled-components'
+import GlobalContent from "./Components/Structurals/GlobalContent.component";
+import GlobalFooterComponent from "./Components/Structurals/GlobalFooter.component";
+import GlobalHeaderComponent from "./Components/Structurals/GlobalHeader.component";
+
+const GlobalStructureComponent = styled.div`
+    border: 2px solid #fcfcfc;
+    border-radius: 10px;
+    max-width: 900px;
+    height: 700px;
+
+    overflow: hidden;
+
+    background-color: #EEEEEE;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`;
 
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalStructure />
+    <BrowserRouter basename='' >
+      <GlobalStructureComponent>
+            <GlobalHeaderComponent />
+
+            <GlobalContent />
+            
+            <GlobalFooterComponent />
+        </GlobalStructureComponent>
     </BrowserRouter>
   );
 }
